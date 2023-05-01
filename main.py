0f721d531e5b8027cd405410fb137862a1068959
KwonSungyeal <33076172+KwonSungyeal@users.noreply.github.com>
2023-05-02 07:36:01 +0900

    conf pre-commit-config black

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 # Test python env
 def print_hello():
-    animals = ['dog','cat','hamster','tiger'] # in ont line
-    foods = [
-            'Spagetti'
-            'Pizza',
-            'bibimbob'
-            ] # w/o tariling comma
+    animals = ["dog", "cat", "hamster", "tiger"]  # in ont line
+    foods = ["Spagetti" "Pizza", "bibimbob"]  # w/o tariling comma
     names = [
-            'John',
-            'Jane',
-            'Gil-dong',
-            'Dong-eun',
-            ] # w/ trailing comma
+        "John",
+        "Jane",
+        "Gil-dong",
+        "Dong-eun",
+    ]  # w/ trailing comma
     for f_name in names:
-        print(f'hello,{f_name}')
+        print(f"hello,{f_name}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print_hello()
